5371f86207712c95d5ec3cdf6dc5eba885d11621
user <kalyakulin.semen@gmail.com>
2022-07-24 21:28:19 +0700

    Connected the server db (seems to be working)

--- PersonalSite/settings.py
+++ PersonalSite/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -29,8 +28,9 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     '::1',
+    'f89082zi.beget.tech',
+    'www.f89082zi.beget.tech',
 ]
-
 
 # Application definition
 
@@ -75,20 +75,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'PersonalSite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'personal_site',
+        # 'USER': 'root',
+        # 'PASSWORD': '1771',
+        # 'HOST': 'localhost',
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'personal_site',
-        'USER': 'root',
-        'PASSWORD': '1771',
-        'HOST': 'localhost',
+        'NAME': 'f89082zi_persona',
+        'USER': 'f89082zi_persona',
+        'PASSWORD': 'gqIuFxM2',
+        'HOST': 'f89082zi.beget.tech',
+        'Port': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -108,7 +112,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -119,7 +122,6 @@ TIME_ZONE = 'Europe/Moscow'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
